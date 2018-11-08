963d2f0b53790263f6728033cbed3d3797fedab5
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2018-11-07 18:14:53 -0700

    working on curry

--- reduce.js
+++ reduce.js
@@ -27,6 +27,8 @@ items.reduce(function (acc, cur) {
 
 //https://slides.com/meeple142/javascript-arrays-9#/11/8
 
+//Flatten Arrays
+
 //ES6
 [
     [1, 2, 3],
@@ -45,6 +47,8 @@ items.reduce(function (acc, cur) {
 
 
 //https://slides.com/meeple142/javascript-arrays-9#/11/9
+
+//Split Arrays
 
 //ES6
 const splitEvery = (chunkSize, arr) => (
@@ -67,6 +71,10 @@ function splitEvery(chunkSize, arr) {
 
 //https://slides.com/meeple142/javascript-arrays-9#/11/10
 
+//Compose Functions
+
+//Curry Functions
+
 //ES6
 const compose = (...fns) => (
     (x) => fns.reduceRight(
@@ -76,12 +84,30 @@ const compose = (...fns) => (
 );
 
 //ES5
+function addOne(num) {
+    return num + 1;
+}
 
-//EXAMPLE NOT DONE YET
-function compose(fns) {
+function square(num) {
+    return num * num
+}
 
+function roundToNearestTen(num) {
+    return (num % 10 < 5) ? (num - num % 10) : num + (10 - num % 10);
+}
+
+var myNumberFunctions = [addOne, square, roundToNearestTen]
+
+function compose(functions, initialInput) {
+    return functions.reduce(function (acc, curr) {
+        return curr(acc);
+    }, initialInput);
 
 }
+
+console.log("Expected Output: 60", compose(myNumberFunctions, 7));
+
+
 
 
 //https://slides.com/meeple142/javascript-arrays-9#/11/11
@@ -97,8 +123,44 @@ const curry = (fn) => (
     )
 );
 
+//USE IF
+const curry = function (fn) {
+    return function (...args) {
+        return fn.length <= 1 || args.length >= fn.length ?
+            fn(...args) :
+            args.reduce(function (acc, curr) {
+                return curry(acc.bind(null, curr));
+            }, fn);
+    }
+};
+
+hi(a, b, c, d, e)
+cHi = curry(hi);
+first2hi = cHi(1, 2);
+first2hi(3, 4, 5)
+
+
+const sum = x => y => x + y;
+// returns the number 3
+sum(2)(1);
+// returns a function y => 2 + y
+var add2 = sum(2);
+
+add2(12)
+
 //ES5
-//EXAMPLE NOT DONE YET
+function curry(fn) {
+    if (fn.length <= 1 || arguments.length >= fn.length) {
+        fn(arguments)
+    } else {
+        Array.prototype.reduce.call(arguments, function (acc, current) {
+            curry(acc.bind(null, current)), fn
+        });
+    }
+
+
+
+}
 
 
 //https://slides.com/meeple142/javascript-arrays-9#/11/12
